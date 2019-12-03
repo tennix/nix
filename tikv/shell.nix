9088ab0ca8aade77bfeeabaf31484949d35d8581
tennix <ztennix@gmail.com>
2019-12-03 23:19:11 +0800

    simplify nix expression

--- tikv/shell.nix
+++ tikv/shell.nix
@@ -1,18 +1,17 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  nightlyRust = (nixpkgs.rustChannelOf {
+    date = "2019-09-05";
+    channel = "nightly";
+  }).rust.override {
+    extensions = [
+      "clippy-preview"
+      "rustfmt-preview"
+    ];
+  };
 in
 with nixpkgs;
-let nightlyRust = (nixpkgs.rustChannelOf {
-      date = "2019-09-05";
-      channel = "nightly";
-    }).rust.override {
-      extensions = [
-        "clippy-preview"
-        "rustfmt-preview"
-      ];
-    };
-in
 stdenv.mkDerivation {
   name = "tikv_build_shell";
   buildInputs = [
